19f380aadc513942da5773188d3ade2ba85f14a8
Алексей <sayhell042@gmail.com>
2024-09-01 14:53:39 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       double a = 4.7;
+       double b = 3.8;
+       double sum = a + b;
+        System.out.println(sum);
+       double dif = a - b;
+        System.out.println(dif);
+       double prod = a * b;
+        System.out.println(prod);
+       double del = a / b;
+        System.out.println(del);
+       double zer = a % b;
+        System.out.println(zer);
+        BigDecimal bigDecimal1 = new BigDecimal(4.7);
+        BigDecimal bigDecimal2 = new BigDecimal(3.8);
+        BigDecimal bigDecimalProd = new BigDecimal();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
     }
 }
